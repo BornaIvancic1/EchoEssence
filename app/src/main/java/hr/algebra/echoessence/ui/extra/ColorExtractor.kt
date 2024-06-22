39c201d0b2062c826c5392a28ae241ad329664d7
BornaIvancic1 <bornaivancic2001@outlook.com>
2024-06-22 16:20:58 +0200

    Design improvements

---
+++ app/src/main/java/hr/algebra/echoessence/ui/extra/ColorExtractor.kt
@@ -0,0 +1,36 @@
+package hr.algebra.echoessence.ui.extra
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Color
+import androidx.palette.graphics.Palette
+
+class ColorExtractor(private val context: Context) {
+
+    fun extractColors(bitmap: Bitmap, listener: ColorExtractionListener) {
+        Palette.from(bitmap).generate { palette ->
+            palette?.let {
+                val defaultColor = context.resources.getColor(android.R.color.black)
+                val dominantColor = palette.getDominantColor(defaultColor)
+                val vibrantColor = palette.getVibrantColor(defaultColor)
+
+                val darkenedDominantColor = darkenColor(dominantColor, 0.8f)
+                val darkenedVibrantColor = darkenColor(vibrantColor, 0.8f)
+
+                listener.onColorsExtracted(darkenedDominantColor, darkenedVibrantColor)
+            }
+        }
+    }
+
+    private fun darkenColor(color: Int, factor: Float): Int {
+        val a = Color.alpha(color)
+        val r = Math.round(Color.red(color) * factor).coerceAtMost(255)
+        val g = Math.round(Color.green(color) * factor).coerceAtMost(255)
+        val b = Math.round(Color.blue(color) * factor).coerceAtMost(255)
+        return Color.argb(a, r, g, b)
+    }
+
+    interface ColorExtractionListener {
+        fun onColorsExtracted(darkenedDominantColor: Int, darkenedVibrantColor: Int)
+    }
+}
